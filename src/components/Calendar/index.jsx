b7ce9998436c84470fa9285efc10b0bd9bff9e1c
Hokyung (Andy) Lee <techandy42@gmail.com>
2023-01-07 21:44:53 -0600

    cleaned up embeded calendar code

--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -1,25 +1,25 @@
-import { useEffect } from 'react'
 import { useCalendarId } from 'hooks'
 
 export const Calendar = () => {
-  const { calendarId } = useCalendarId()
-
-  useEffect(() => {
-    console.log('calendarId', calendarId)
-  }, [calendarId])
+  const { calendarId, loading } = useCalendarId()
 
   return (
     <div className='task-list__wrapper'>
       <h1>My Calendar</h1>
       {calendarId ? (
         <iframe
+          title='calendar'
           src={`https://calendar.google.com/calendar/embed?
 src=${calendarId.replace('@', '%40')}&
 ctz=America%2FWinnipeg`}
-          style={{ border: 0 }}
-          width='800'
-          height='600'
+          style={{
+            border: 0,
+            width: 800,
+            height: 600,
+          }}
         ></iframe>
+      ) : loading ? (
+        <h4>Calendar Loading</h4>
       ) : (
         <h4>Calendar Unavailable</h4>
       )}
