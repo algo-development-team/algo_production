38ccb0117bcb381d6b206849e7ba939ee5747ef5
Hokyung (Andy) Lee <techandy42@gmail.com>
2023-01-07 20:12:25 -0600

    fixed the default project routing issues

--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -1,7 +1,6 @@
 import { Board } from 'components/BoardView/index'
 import { TaskList } from 'components/ListView'
 import { useSelectedProjectInfo } from 'hooks'
-import React from 'react'
 import { useParams } from 'react-router-dom'
 import './styles/content.scss'
 import './styles/light.scss'
@@ -10,11 +9,27 @@ export const Content = () => {
   const projectInfo = useSelectedProjectInfo(projectId)
   const currentView = projectInfo && projectInfo[0]?.projectIsList
 
+  const getProject = () => {
+    if (defaultGroup) {
+      if (
+        defaultGroup === 'Checklist' ||
+        defaultGroup === 'Inbox' ||
+        defaultGroup === 'Scheduled'
+      ) {
+        return <TaskList />
+      } else if (defaultGroup === 'Calendar') {
+        return <TaskList />
+      } else {
+        return null
+      }
+    } else {
+      return currentView ? <TaskList /> : <Board />
+    }
+  }
+
   return (
     <div className='content'>
-      <div className='project__wrapper'>
-        {currentView || defaultGroup ? <TaskList /> : <Board />}
-      </div>
+      <div className='project__wrapper'>{getProject()}</div>
     </div>
   )
 }
