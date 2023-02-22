6455afbb6b35fe16863b5b78c973c3c2d6a94375
Hokyung (Andy) Lee <techandy42@gmail.com>
2023-02-22 13:04:43 -0500

    converted inline-css in schedule-header to css

--- src/components/Schedule/schedule-header.jsx
+++ src/components/Schedule/schedule-header.jsx
@@ -8,69 +8,29 @@ import {
 import { NavLink } from 'react-router-dom'
 import { useState } from 'react'
 import { useResponsiveSizes } from 'hooks'
+import './styles/light.scss'
+import './styles/main.scss'
 
 export const ScheduleHeader = ({ dayId }) => {
   const [isWeeklyView, setIsWeeklyView] = useState(false)
   const { sizes } = useResponsiveSizes()
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: sizes.smallPhone ? 'center' : 'space-between',
-        alignItems: 'center',
-        width: '100%',
-      }}
-    >
+    <div className={`schedule-header${sizes.smallPhone ? '-small-phone' : ''}`}>
       {!sizes.smallPhone && (
-        <button
-          style={{
-            color: 'inherit',
-            padding: '10px',
-            fontSize: '16px',
-            borderRadius: '5px',
-            backgroundColor: '#282828',
-            width: '120px',
-          }}
-        >
+        <button className='schedule-header__menu-btn'>
           {formatDate(dayId)}
         </button>
       )}
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <p
-          style={{
-            fontSize: '14px',
-            fontWeight: 'bold',
-            color: '#3f4cda',
-            marginBottom: '2px',
-          }}
-        >
+      <div className='schedule-header__date-btn-container'>
+        <p className='schedule-header__date-btn-subtitle'>
           {getDayOfWeek(dayId)}
         </p>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div className='schedule-header__date-btn-icon-container'>
           <NavLink to={`/schedule/${getDayId(getDayDiff(dayId) - 1)}`}>
             <i class='arrow-xl left' />
           </NavLink>
-          <button
-            style={{
-              width: '40px',
-              height: '40px',
-              borderRadius: '50%',
-              color: 'white',
-              fontSize: '16px',
-              fontWeight: 600,
-              marginLeft: '5px',
-              marginRight: '5px',
-              backgroundColor: '#3f4cda',
-              cursor: 'default',
-            }}
-          >
+          <button className='schedule-header__date-btn-icon'>
             {getDay(dayId)}
           </button>
           <NavLink to={`/schedule/${getDayId(getDayDiff(dayId) + 1)}`}>
@@ -80,14 +40,7 @@ export const ScheduleHeader = ({ dayId }) => {
       </div>
       {!sizes.smallPhone && (
         <button
-          style={{
-            color: 'inherit',
-            padding: '10px',
-            fontSize: '16px',
-            borderRadius: '5px',
-            backgroundColor: '#282828',
-            width: '120px',
-          }}
+          className='schedule-header__menu-btn'
           onClick={() => setIsWeeklyView(!isWeeklyView)}
         >
           {isWeeklyView ? 'Daily' : 'Weekly'}
