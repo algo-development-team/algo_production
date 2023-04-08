839f897fcf15874fc7171f0172d09f0f4bc09d1c
Hokyung (Andy) Lee <techandy42@gmail.com>
2023-04-08 06:56:56 -0400

    Updated the sidebar code

--- src/components/Sidebar/custom-project.jsx
+++ src/components/Sidebar/custom-project.jsx
@@ -1,20 +1,11 @@
 import { ReactComponent as Dot } from 'assets/svg/dot.svg'
 import { OptionsButton } from 'components/MenuButton'
 import { useEffect, useState } from 'react'
-import { NavLink, useParams } from 'react-router-dom'
-import { useProjects, useSelectedProject, useTasks } from '../../hooks'
+import { NavLink } from 'react-router-dom'
 import { ProjectTasksCounts } from './project-tasks-count'
 import { cropLabel } from 'handleLabel'
 export const CustomProject = ({ project }) => {
-  const params = useParams()
-  const { projects } = useProjects()
-  const { setSelectedProject, selectedProject } = useSelectedProject(
-    params,
-    projects,
-  )
-  // const current = useSelectedProjectValue().selectedProject.selectedProjectId;
   const [currentTaskProp, setCurrentTaskProp] = useState([])
-  const { setTasks } = useTasks()
   useEffect(() => {
     setCurrentTaskProp(project)
   }, [project])
@@ -26,14 +17,9 @@ export const CustomProject = ({ project }) => {
         isActive ? 'active project-group' : 'project-group'
       }
       role='button'
-      onClick={() => {
-        // setSelectedProject({ selectedProjectName: project.name, selectedProjectId: project.projectId, ...project });
-      }}
     >
       <div className='project-group__group'>
         <div className='project-group__icon'>
-          {/* <div className="custom-project__dot" style={{ backgroundColor: `${project?.projectColour?.hex}` }}></div> */}
-          {/* <InboxIcon fill="#246fe0" /> */}
           <Dot color={`${project?.projectColour?.hex}`} />
         </div>
         <p className='project-group__name'>
