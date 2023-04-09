2f8cab9da4f89c7afe454bd2e36dd68044c1e6e6
Hokyung (Andy) Lee <techandy42@gmail.com>
2023-04-09 00:56:05 -0400

    set-up content for new pages

--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -3,33 +3,33 @@ import { TaskList } from 'components/ListView'
 import { useSelectedProjectInfo } from 'hooks'
 import { useParams } from 'react-router-dom'
 import { FullCalendar } from 'components/FullCalendar'
-import { Schedule } from 'components/Schedule'
 import './styles/content.scss'
 import './styles/light.scss'
 import React from 'react'
 
 export const Content = () => {
-  const { defaultGroup, projectId } = useParams()
+  const { defaultGroup, projectId, scheduleId } = useParams()
   const projectInfo = useSelectedProjectInfo(projectId)
   const currentView = projectInfo && projectInfo[0]?.projectIsList
 
   const getProject = () => {
     if (defaultGroup) {
-      if (
-        defaultGroup === 'Checklist' ||
-        defaultGroup === 'Inbox' ||
-        defaultGroup === 'Scheduled'
-      ) {
-        return <TaskList />
-      } else if (defaultGroup === 'Calendar') {
+      if (defaultGroup === 'Calendar') {
         return <FullCalendar />
-      } else if (defaultGroup === 'Schedule') { // remove later
-         return <Schedule />
+      } else if (defaultGroup === 'Overview') {
+        // return <Overview />
+        return null
+      } else if (defaultGroup === 'Setting') {
+        // return <Setting />
+        return null
       } else {
         return null
       }
     } else if (projectId) {
       return currentView ? <TaskList /> : <Board />
+    } else if (scheduleId) {
+      // return <ScheduleCalendar />
+      return null
     } else {
       return null
     }
