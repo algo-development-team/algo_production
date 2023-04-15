09715194c90fc919919c65eb94f20be501edfbe1
Hokyung (Andy) Lee <techandy42@gmail.com>
2023-04-15 18:06:29 -0400

    fixed the dark mode not persisting problem

--- src/context/theme-context.js
+++ src/context/theme-context.js
@@ -4,23 +4,14 @@ export const ThemeContext = createContext()
 
 export const ThemeContextProvider = ({ children }) => {
   const theme = localStorage.getItem('algo_theme')
-  const [isLight, setIsLight] = useState(theme === 'DARK' ? false : true)
-
-  const darkThemeMq = window.matchMedia('(prefers-color-scheme: dark)')
-
-  useEffect(() => {
-    if (darkThemeMq.matches) {
-      setIsLight(false)
-      localStorage.setItem('algo_theme', 'DARK')
-    } else {
-      setIsLight(true)
-      localStorage.setItem('algo_theme', 'LIGHT')
-    }
-  }, [])
+  const [isLight, setIsLight] = useState(
+    theme && theme === 'DARK' ? false : true,
+  )
 
   useEffect(() => {
     localStorage.setItem('algo_theme', isLight ? 'LIGHT' : 'DARK')
   }, [isLight])
+
   return (
     <ThemeContext.Provider value={{ isLight, setIsLight }}>
       {children}
